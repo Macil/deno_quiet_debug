54ccddd1cbf96e402fe02c51bb7e7c8e97a02c0c
Chris Cowan <agentme49@gmail.com>
2022-12-11 20:45:40 -0800

    use async modules to remove possibility of async logging

--- debugEnvPermissionStatus.ts
+++ debugEnvPermissionStatus.ts
@@ -1,15 +1,4 @@
-import once from "https://deno.land/x/once@0.3.0/index.ts";
-
-export let debugEnvPermissionStatus: Deno.PermissionStatus | undefined =
-  undefined;
-
-export const getDebugEnvPermissionStatus = once(() =>
-  Deno.permissions.query({
-    name: "env",
-    variable: "DEBUG",
-  })
-    .then((status) => {
-      debugEnvPermissionStatus = status;
-      return status;
-    })
-);
+export const debugEnvPermissionStatus = await Deno.permissions.query({
+  name: "env",
+  variable: "DEBUG",
+});
--- mod.ts
+++ mod.ts
@@ -1,71 +1,25 @@
 import { debug as debug_ } from "https://deno.land/x/debug@0.2.0/mod.ts";
-import {
-  debugEnvPermissionStatus,
-  getDebugEnvPermissionStatus,
-} from "./debugEnvPermissionStatus.ts";
+import { debugEnvPermissionStatus } from "./debugEnvPermissionStatus.ts";
 
 export interface Debug {
   (fmt: string, ...args: unknown[]): void;
 }
 
-export interface Options {
-  /**
-   * If true, warn when a log message is delivered asynchronously.
-   * @defaultValue `true`
-   */
-  warnOnAsyncLog?: boolean;
-}
-
-export function debug(namespace: string, options?: Options): Debug {
-  if (debugEnvPermissionStatus) {
-    return debugUsingDebugEnvPermissionStatus(
-      namespace,
-      debugEnvPermissionStatus,
-    );
-  } else {
-    let logger: Debug;
-
-    const loggerFromPermStatusPromise = getDebugEnvPermissionStatus()
-      .then((status) => {
-        logger = debugUsingDebugEnvPermissionStatus(namespace, status);
-        return logger;
-      });
-
-    logger = (...args) => {
-      loggerFromPermStatusPromise.then((logger) => {
-        logger(...args);
-        if (options?.warnOnAsyncLog !== false) {
-          // We give this warning on the same logger as the log message so
-          // it doesn't show up if the logger is disabled.
-          logger(
-            "quiet-debug warning: previous log message was delivered asynchronously",
-          );
-        }
-      });
-    };
-
-    return (...args) => logger(...args);
-  }
-}
-
-function debugUsingDebugEnvPermissionStatus(
-  namespace: string,
-  status: Deno.PermissionStatus,
-): Debug {
-  if (status.state === "granted") {
+export function debug(namespace: string): Debug {
+  if (debugEnvPermissionStatus.state === "granted") {
     return debug_(namespace);
-  } else if (status.state === "denied") {
+  } else if (debugEnvPermissionStatus.state === "denied") {
     return () => {};
   } else {
-    // status.state is "prompt". Support the possibility of the
-    // permission changing later.
+    // debugEnvPermissionStatus.state is "prompt". Support the
+    // possibility of the permission changing later.
     let logger: Debug;
 
     logger = (...args) => {
-      if (status.state === "granted") {
+      if (debugEnvPermissionStatus.state === "granted") {
         logger = debug_(namespace);
         logger(...args);
-      } else if (status.state === "denied") {
+      } else if (debugEnvPermissionStatus.state === "denied") {
         logger = () => {};
       }
     };
